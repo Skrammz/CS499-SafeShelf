1bc236a14f573576b1104cfd6638eb4f7541e24c
Skrammz <dessabiddle@gmail.com>
2024-04-19 12:52:10 -0500

    latestage changes

---
+++ backend.py
@@ -0,0 +1,47 @@
+import os
+import pandas as pd
+import sys
+
+sys.path.insert(0, r"c:\users\user\appdata\local\programs\python\python39\lib\site-packages")
+
+f = open("txt.txt", "r")
+temp = f.readline()
+os.system(temp)
+def column_switch(df, column1, column2):
+    i = list(df.columns)
+    a, b = i.index(column1), i.index(column2)
+    i[b], i[a] = i[a], i[b]
+    df.columns = i
+    return df
+def mainFunc():
+	with open("./modified.json") as f:
+		temp = pd.read_json(f)
+		temp.to_csv("data_file.csv", index=False)
+def nameChange():
+        with open("./hi.json") as f:
+                data = pd.read_json(f)
+                header_mapping = {'field_title': 'Title',
+                                  'field_last_modified_date' : 'Date Issued',
+                                  'field_active_notice': 'Recall Status',
+                                  'field_states': 'States',
+                                  'field_establishment': 'Company',
+                                  'field_recall_classification': 'Recall Classification',
+                                  'field_recall_reason': 'Recall Reason'}
+                data = data.drop(["field_archive_recall", "field_closed_date",
+                                  "field_closed_year", "field_company_media_contact",
+                                  "field_closed_date", "field_distro_list",
+                                  "field_en_press_release", "field_labels",
+                                  "field_media_contact", "field_recall_date",
+                                  "field_press_release", "field_processing",
+                                  "field_product_items", "field_qty_recovered",
+                                  "field_closed_date", "field_recall_number",
+                                  "field_recall_type", "field_related_to_outbreak",
+                                  "field_summary", "field_year", "langcode",
+                                  "field_has_spanish", "field_risk_level"], axis = 1)
+                data = data.rename(columns = header_mapping)
+                columns_titles = ["Company", "Title", "States", "Date Issued", "Recall Reason",
+                                  "Recall Status", "Recall Classification"]
+                data = data.reindex(columns = columns_titles)
+                data.to_json('modified.json', orient = 'records', indent = 4)
+nameChange()
+mainFunc()
---
+++ genMapData.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import pandas as pd
+
+nationWideActive = 0
+nationWideClosed = 0
+activeStates = []
+closedStates = []
+
+states = [
+['Alabama','AL',0,0],
+['Alaska','AK',0,0],
+['Arizona','AZ',0,0],
+['Arkansas','AR',0,0],
+['California','CA',0,0],
+['Colorado','CO',0,0],
+['Connecticut','CT',0,0],
+['Delaware','DE',0,0],
+['District of Columbia','DC',0,0],
+['Florida','FL',0,0],
+['Georgia','GA',0,0],
+['Hawaii','HI',0,0],
+['Idaho','ID',0,0],
+['Illinois','IL',0,0],
+['Indiana','IN',0,0],
+['Iowa','IA',0,0],
+['Kansas','KS',0,0],
+['Kentucky','KY',0,0],
+['Louisiana','LA',0,0],
+
+['Maine','ME',0,0],
+['Maryland','MD',0,0],
+['Massachusetts','MA',0,0],
+['Michigan','MI',0,0],
+['Minnesota','MN',0,0],
+['Mississippi','MS',0,0],
+['Missouri','MO',0,0],
+['Montana','MT',0,0],
+['Nebraska','Ne',0,0],
+['Nevada','NV',0,0],
+['Missouri','MO',0,0],
+['Montana','MT',0,0],
+['Nebraska','NE',0,0],
+['Nevada','NV',0,0],
+['New Hampshire','NH',0,0],
+['New Jersey','NJ',0,0],
+['New Mexico','NM',0,0],
+['New York','NY',0,0],
+['North Carolina','NC',0,0],
+['North Dakota','ND',0,0],
+['Ohio','OH',0,0],
+['Oklahoma','OK',0,0],
+['Oregon','OR',0,0],
+['Pennsylvania','PA',0,0],
+['Puerto Rico','PR',0,0],
+['Rhode Island','RI',0,0],
+['South Carolina','SC',0,0],
+['South Dakota','SD',0,0],
+['Tennessee','TN',0,0],
+['Texas','TX',0,0],
+['Utah','UT',0,0],
+['Vermont','VT',0,0],
+['Virginia','VA',0,0],
+['Washington','WA',0,0],
+['West Virginia','WV',0,0],
+['Wisconsin','WI',0,0],
+['Wyoming','WY',0,0],
+]
+
+def convert():
+    with open("./hi.json") as f:
+        temp = pd.read_json(f)
+        temp.to_csv("hicsv.csv", index=False)
+convert() 
+                
+def readCSV2():
+    with open("hicsv.csv") as df:
+        for i in df[df.columns[23]]:
+            state = df[df.columns[3]]
+            if str(i) == "Active Recall":
+                activeStates.append(state)
+            elif str(i) == "Closed Recall":
+                closedStates.append(state)
+ 
+def readCSV():
+    with open("hicsv.csv", encoding = "utf8") as df:
+        temp = pd.read_csv(df)
+        for i in temp[temp.columns[23]]:
+            state = temp[temp.columns[3]]
+            if str(i) == "Active Recall":
+                activeStates.append(state)
+            elif str(i) == "Closed Recall":
+                closedStates.append(state)
+
+def writeActive():
+    for x in activeStates:
+        if type(x) != float:
+            if "Nationwide" in x:
+                nationWideActive += 1
+            else:
+                for i in range(len(states)):
+                    states[i][2] = x.count()
+                    for stateName in x:
+                        states[i][2] += 1
+
+readCSV()
+writeActive()
+print(states)
